6c325cc246f3c986e3f7ea48d27efe726208c4f5
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-01-19 23:03:25 +0700

    video 25

--- src/server.js
+++ src/server.js
@@ -1,31 +1,20 @@
+require("dotenv").config();
 const express = require("express"); //commonjs
 const path = require("path"); //commonjs
-// import express from "express"; //es modules
-
-require("dotenv").config();
-console.log(">>> check env: ", process.env);
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port hardcode .uat .prod
 const hostname = process.env.HOST_NAME;
 
 // config template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+configViewEngine(app);
+
+//config static files
 
 //khai bao route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/abc", (req, res) => {
-  res.send("check ABC");
-});
-
-app.get("/hoidanit", (req, res) => {
-  // res.send("<h1>hoi dan it with eric</h1>");
-  res.render("sample.ejs");
-});
+app.use("/", webRoutes);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
